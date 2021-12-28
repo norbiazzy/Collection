113f66a28605ba547839686328908d03b1fc8f38
Vadim Galakov <vadimstar123321@mail.ru>
2021-12-28 20:48:02 +0300

    Poroxy

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ const PORT = config.get('port') || 5000
 
 app.use('/api/auth', require('./routes/auth.routes'))
 
-async function start(){
+async function start() {
   try {
     await mongoose.connect(config.get('mongoUri'), {
-      useNewUrlParser: true,
-       useUndefinedTopology: true,
-      useCreateIndex: true
+      useNewUrlParser: true
     })
-  
-    app.listen(5000, ()=>{
+    app.listen(PORT, () => {
       console.log(`app has been started on port ${PORT}...`)
     })
   } catch (e) {
     console.log(e)
   }
 }
+
+start()
