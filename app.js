f7f693022b89b6cfb5cb7edd572450680d7f6b03
Vadim Galakov <vadimstar123321@mail.ru>
2022-01-20 19:45:11 +0300

    'heroku-fix'

--- app.js
+++ app.js
@@ -4,34 +4,33 @@ const mongoose = require('mongoose')
 const path = require('path')
 
 const app = express();
-app.use(express.json({ extended: true }))
+app.use(express.json({extended: true}))
 app.use('/api/profile', require('./routes/profile.routes'))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/collection', require('./routes/collection.routes'))
 app.use('/api/home', require('./routes/home.routes'))
 
-const PORT =  process.env.PORT || config.get('port') || 5000
+const PORT = process.env.PORT || config.get('port') || 5000
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-    
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
-
-
-async function start() {
+  
+  async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'), {
-            useNewUrlParser: true
-        })
-        app.listen(PORT, () => {
-            console.log(`app has been started on port ${PORT}...`)
-        })
+      await mongoose.connect(config.get('mongoUri'), {
+        useNewUrlParser: true
+      })
+      app.listen(PORT, () => {
+        console.log(`app has been started on port ${PORT}...`)
+      })
     } catch (e) {
-        console.log(e)
+      console.log(e)
     }
-}
-
-start()
+  }
+  
+  start()
